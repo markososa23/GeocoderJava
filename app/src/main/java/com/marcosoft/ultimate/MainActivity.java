4c38cf9db1f2e1d5079dad3313699e8f5d74e75d
markososa23 <markososa23@gmail.com>
2020-08-12 13:00:05 -0300

    actualizacion con envio de coordenadas a mysql

--- app/src/main/java/com/marcosoft/ultimate/MainActivity.java
+++ app/src/main/java/com/marcosoft/ultimate/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,30 +14,84 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView latitud,longitud;
-    TextView direccion;
+    TextView mensaje1;
+    TextView mensaje2;
+     Button botonGuardar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        latitud = (TextView) findViewById(R.id.txtLatitud);
-        longitud = (TextView) findViewById(R.id.txtLongitud);
-        direccion = (TextView) findViewById(R.id.txtDireccion);
+        mensaje1 = (TextView) findViewById(R.id.mensaje_id);
+        mensaje2 = (TextView) findViewById(R.id.mensaje_id2);
+        botonGuardar = (Button) this.findViewById(R.id.btnGuardar);
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutarServicio("http://192.168.0.15/gpsbd/insertar.php");
+            }
+        });
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
         } else {
             locationStart();
         }
+    }
+
+    public void ejecutarServicio(String URL){
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Operacion exitosa"+response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> parametros= new HashMap<>();
+                parametros.put("coordenadas",mensaje1.getText().toString());
+                parametros.put("direccion",mensaje2.getText().toString());
+                return  parametros;
+
+            }
+      /*     @Override
+           public Map getHeaders() {
+               HashMap headers = new HashMap();
+               headers.put("Content-Type", "text/plain; charset=utf-8");
+               headers.put("codProducto",txtCodigo.getText().toString());
+               headers.put("descripcion",txtDescripcion.getText().toString());
+               headers.put("precio",txtPrecio.getText().toString());
+               return headers;
+           }*/
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     private void locationStart() {
@@ -52,15 +107,16 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
             return;
         }
-        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
-        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
-        latitud.setText("Localización agregada");
-        direccion.setText("");
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, Local);
+        mensaje1.setText("Localizacion agregada");
+        mensaje2.setText("");
     }
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == 1000) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 locationStart();
+                return;
             }
         }
     }
@@ -73,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                         loc.getLatitude(), loc.getLongitude(), 1);
                 if (!list.isEmpty()) {
                     Address DirCalle = list.get(0);
-                    direccion.setText(DirCalle.getAddressLine(0));
+                    mensaje2.setText(DirCalle.getAddressLine(0));
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -95,21 +151,19 @@ public class MainActivity extends AppCompatActivity {
             // debido a la deteccion de un cambio de ubicacion
             loc.getLatitude();
             loc.getLongitude();
-            String sLatitud = String.valueOf(loc.getLatitude());
-            String sLongitud = String.valueOf(loc.getLongitude());
-            latitud.setText(sLatitud);
-            longitud.setText(sLongitud);
+            String Text = "Lat = "+ loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            mensaje1.setText(Text);
             this.mainActivity.setLocation(loc);
         }
         @Override
         public void onProviderDisabled(String provider) {
             // Este metodo se ejecuta cuando el GPS es desactivado
-            latitud.setText("GPS Desactivado");
+            mensaje1.setText("GPS Desactivado");
         }
         @Override
         public void onProviderEnabled(String provider) {
             // Este metodo se ejecuta cuando el GPS es activado
-            latitud.setText("GPS Activado");
+            mensaje1.setText("GPS Activado");
         }
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -127,4 +181,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
